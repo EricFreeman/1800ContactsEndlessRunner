f7c517cd946529be375ffc3203df252f06a1ca75
Eric Freeman <clownhunter@gmail.com>
2015-03-26 14:46:03 -0600

    Added customer asset and made player jump.

---
+++ src/Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Jump : MonoBehaviour
+    {
+        public float JumpHeight;
+        public float JumpSpeed;
+
+        private bool _isJumping;
+        private bool _isFalling;
+        private Vector3 _basePosition;
+        private Vector3 _jumpPinnacle;
+
+        void Start()
+        {
+            _basePosition = transform.position;
+            _jumpPinnacle = transform.position + new Vector3(0, JumpHeight, 0);
+        }
+
+        void Update()
+        {
+            if (!_isJumping && Input.GetKeyDown(KeyCode.Space))
+            {
+                _isJumping = true;
+            }
+
+            if (_isJumping)
+            {
+                if (!_isFalling)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, _jumpPinnacle, JumpSpeed*Time.deltaTime);
+
+                    if (transform.position == _jumpPinnacle)
+                    {
+                        _isFalling = true;
+                    }
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, _basePosition, JumpSpeed * Time.deltaTime);
+
+                    if (transform.position == _basePosition)
+                    {
+                        _isJumping = false;
+                        _isFalling = false;
+                    }
+                }
+            }
+        }
+    }
+}
