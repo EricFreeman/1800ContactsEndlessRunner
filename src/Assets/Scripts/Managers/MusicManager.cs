9b13d1de42b1bc09715d6c1b31d1caf524fa0442
Eric Freeman <clownhunter@gmail.com>
2015-03-26 12:06:19 -0600

    Added in some assets and got background music playing.

---
+++ src/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class MusicManager : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+            if (!_audioSource.isPlaying)
+            {
+                // TODO: Support more than one song
+                _audioSource.Play();
+            }
+        }
+    }
+}
