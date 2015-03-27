809c0a1b922a424e8cbc204a6f60bbf77f17850d
Eric Freeman <clownhunter@gmail.com>
2015-03-26 22:22:15 -0600

    Updated logo to flash press space or tap to start depending on device.

--- src/Assets/Scripts/UI/MainMenu.cs
+++ src/Assets/Scripts/UI/MainMenu.cs
@@ -7,12 +7,50 @@ namespace Assets.Scripts.UI
     public class MainMenu : MonoBehaviour
     {
         private bool _isPlaying;
+        private bool _isPhone;
+        private bool _isFlashing;
+
+        public int AnimationDelay = 12;
+        private int _currentAnimationDelay;
+
+        public Sprite Logo;
+        public Sprite LogoSpaceToStart;
+        public Sprite LogoTapToStart;
+
+        void Start()
+        {
+            #if UNITY_ANDROID
+                _isPhone = true;
+            #elif UNITY_IPHONE
+                _isPhone = true;
+            #else
+                _isPhone = false;
+            #endif
+
+            _currentAnimationDelay = AnimationDelay;
+        }
 
         void Update()
         {
+            _currentAnimationDelay--;
+            if (_currentAnimationDelay <= 0 && !_isPlaying)
+            {
+                _currentAnimationDelay = AnimationDelay;
+                _isFlashing = !_isFlashing;
+                var spriteToUse = Logo;
+
+                if (_isFlashing)
+                {
+                    spriteToUse = _isPhone ? LogoTapToStart : LogoSpaceToStart;
+                }
+
+                GetComponent<SpriteRenderer>().sprite = spriteToUse;
+            }
+
             if (Input.GetKeyDown(KeyCode.Space) && !_isPlaying)
             {
                 _isPlaying = true;
+                GetComponent<SpriteRenderer>().sprite = Logo;
                 EventAggregator.SendMessage(new ResumeRunningMessage());
             }
 
