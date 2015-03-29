2e88b6379ae77fb64aa1cff501adda3ce21ebb13
Eric Freeman <clownhunter@gmail.com>
2015-03-28 19:54:43 -0600

    Added first version of new animation controller.

---
+++ src/Assets/Scripts/Shared/AnimationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Shared
+{
+    public class AnimationController : MonoBehaviour
+    {
+        private List<Sprite> _currentAnimation;
+        private List<Sprite> _previousAnimation; 
+
+        public float FramesPerSeconds;
+
+        private int _currentFrame;
+        private float _timeOnFrame;
+
+        void Update()
+        {
+            _timeOnFrame += Time.deltaTime;
+            if (_timeOnFrame >= FramesPerSeconds/60)
+            {
+                _timeOnFrame = 0;
+                _currentFrame++;
+
+                if (_currentFrame >= _currentAnimation.Count)
+                {
+                    _currentFrame = 0;
+                    if (_previousAnimation != null)
+                    {
+                        _currentAnimation = _previousAnimation;
+                        _previousAnimation = null;
+                    }
+                }
+            }
+
+            GetComponent<SpriteRenderer>().sprite = _currentAnimation[_currentFrame];
+        }
+
+        public void PlayAnimation(List<Sprite> anim, bool isOneShot)
+        {
+            if (isOneShot) _previousAnimation = _currentAnimation;
+            _currentAnimation = anim;
+        }
+    }
+}
